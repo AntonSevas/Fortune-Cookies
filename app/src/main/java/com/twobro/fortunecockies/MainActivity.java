7ca7d5b58288001a872b341afd63cd262ae5c2ec
AntonSevas <overton8889@gmail.com>
2022-10-26 14:19:55 +0500

    version 0.1

---
+++ app/src/main/java/com/twobro/fortunecockies/MainActivity.java
@@ -0,0 +1,20 @@
+package com.twobro.fortunecockies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
