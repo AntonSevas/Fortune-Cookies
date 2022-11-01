6abe9cbb83e454d094802e96773d0720cec0ca72
AntonSevas <overton8889@gmail.com>
2022-11-02 03:09:46 +0500

    version 0.2

--- app/src/main/java/com/twobro/fortunecockies/MainActivity.java
+++ app/src/main/java/com/twobro/fortunecockies/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -16,5 +19,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+    }
+    // КНОПКА ОТКРЫТИЯ ПРЕДСКАЗАНИЯ
+    public void openCockie (View view){
+        String predscazanie="You must try, or hate yourself for not trying.";
+
+        ImageView imgv1 = (ImageView) findViewById(R.id.cockie_Image);// Выключаем видимость картинки печенья
+        imgv1.setVisibility(view.INVISIBLE);
+
+
+        TextView predskazanie = (TextView) findViewById(R.id.Cockie);// Включаем видимость предсказания
+        predskazanie.setVisibility(view.VISIBLE);
+
+
+
+        display(predscazanie);
+
+    }
+
+    private void display(String predscazanie) {
+        TextView quantityTextView = findViewById(R.id.Cockie);
+        quantityTextView.setText(predscazanie);
     }
 }
